dad5f222f11703ca3cd61698e42477d82ebf0071
Harvey Ezihe <don4dolex@gmail.com>
2023-11-08 12:03:06 +0000

    Add custom login page

---
+++ src/app/register/form.jsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const RegisterForm = () => {
+	const router = useRouter();
+	const [formData, setFormData] = useState({});
+	const [errorMessage, setErrorMessage] = useState("");
+
+	const handleChange = (e) => {
+		const value = e.target.value;
+		const name = e.target.name;
+
+		if (name === "email") {
+			setFormData((prevState) => ({
+				...prevState,
+				[name]: value.toLowerCase(),
+			}));
+		} else {
+			setFormData((prevState) => ({
+				...prevState,
+				[name]: value,
+			}));
+		}
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setErrorMessage("");
+		const res = await fetch("/api/Users", {
+			method: "POST",
+			body: JSON.stringify({ formData }),
+			"content-type": "application/json",
+		});
+
+		if (!res.ok) {
+			const response = await res.json();
+			setErrorMessage(response.message);
+		} else {
+			router.refresh();
+			router.push("/");
+		}
+	};
+	return (
+		<>
+			<form
+				onSubmit={handleSubmit}
+				method="post"
+				className="flex flex-col gap-3 w-1/2">
+				<h1>Create New User</h1>
+				<label>Full Name</label>
+				<input
+					type="text"
+					id="name"
+					name="fullname"
+					onChange={handleChange}
+					required={true}
+					value={formData.name}
+					className="m-2 bg-slate-400 rounded"
+				/>
+
+				<label>Email</label>
+				<input
+					type="email"
+					name="email"
+					id="email"
+					onChange={handleChange}
+					required={true}
+					value={formData.email}
+					className="m-2 bg-slate-400 rounded"
+				/>
+
+				<label>Password</label>
+				<input
+					type="password"
+					id="password"
+					name="password"
+					onChange={handleChange}
+					required={true}
+					value={formData.password}
+					className="m-2 bg-slate-400 rounded"
+				/>
+				<input
+					type="submit"
+					value="Create User"
+					className="bg-blue-300 hover:bg-blue-100"
+				/>
+			</form>
+			<p className="text-red-600">{errorMessage}</p>
+		</>
+	);
+};
+export default RegisterForm;
